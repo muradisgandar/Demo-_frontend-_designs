6d60a762b4e321e2c12cea431d39eabfc999a5dd
muradisgandar <isgandarli_murad@mail.ru>
2020-10-22 14:57:54 +0400

    modules employee project get all functionaity

--- Modules-Employee Project/src/index.js
+++ Modules-Employee Project/src/index.js
@@ -1,1 +1,45 @@
-console.log("Test");
+import {Request} from "./request";
+import {UI} from "./ui";
+
+const form = document.getElementById("employee-form");
+const nameInput = document.getElementById("name");
+const departmentInput = document.getElementById("department");
+const salaryInput = document.getElementById("salary");
+const employeesList = document.getElementById("employees");
+const updateEmployeeButton = document.getElementById("update");
+
+const request = new Request("http://localhost:3000/employees");
+const ui = new UI();
+
+eventListeners();
+
+function eventListeners(){
+    document.addEventListener("DOMContentLoaded",getAllEmployees);
+}
+
+function getAllEmployees(){
+
+    request.get()
+    .then(employees => {
+        ui.addAllEmployeesToUI(employees);
+    })
+    .catch(err => console.log(err));
+
+}
+
+
+// request.get()
+// .then(employees => console.log(employees))
+// .catch(err => console.log(err));
+
+// request.post({name:"Serhat",department:"Finanice",salary:2000})
+// .then(response => console.log(response))
+// .catch(err => console.log(err));
+
+// request.put(1,{name:"Famil Agayev",department:"Finanice",salary:1000})
+// .then(response => console.log(response))
+// .catch(err => console.log(err));
+
+// request.delete(3)
+// .then(response => console.log(response))
+// .catch(err => console.log(err));
---
+++ Modules-Employee Project/src/ui.js
@@ -0,0 +1,33 @@
+export class UI {
+
+    constructor() {
+        this.nameInput = document.getElementById("name");
+        this.departmentInput = document.getElementById("department");
+        this.salaryInput = document.getElementById("salary");
+        this.employeesList = document.getElementById("employees");
+        this.updateButton = document.getElementById("update");
+    }
+
+    addAllEmployeesToUI(employees) {
+
+        let result = "";
+
+        employees.forEach(employee => {
+            result += `
+            <tr>
+                <td>${employee.name}</td>
+                <td>${employee.department}</td>
+                <td>${employee.salary}</td>
+                <td>${employee.id}</td>
+                <td><a href="#" id = "update-employee" class= "btn btn-danger">Güncelle</a></td> 
+                <td> <a href="#" id = "delete-employee" class= "btn btn-danger">Sil</a></td>
+            </tr>
+            `;
+
+        });
+
+        this.employeesList.innerHTML = result;
+
+
+    }
+}
